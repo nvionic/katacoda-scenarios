71b90a93375d99a505c82262b96d605bff6e7510
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-04-16 16:32:42 -0700

    Moved prompt.sh command to config.sh

--- hello-world/config.sh
+++ hello-world/config.sh
@@ -4,3 +4,11 @@
 mkdir ~/bin
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > ~/bin/machina
 chmod +x ~/bin/machina
+echo "Enter your email:"
+read EMAIL
+echo "Enter your Machina keyspace (Optional):"
+read KEYSPACE
+if [ -z "$KEYSPACE" ]
+then
+    KEYSPACE=Magv
+fi
--- hello-world/prompt.sh
+++ hello-world/prompt.sh
@@ -1,13 +1,2 @@
 #! /usr/bin/env bash
 # comment: use command ./prompt.sh
-echo "Enter your email:"
-read EMAIL
-echo "Enter your Machina keyspace (Optional):"
-read KEYSPACE
-
-if [ -z "$KEYSPACE" ]
-then
-    KEYSPACE=Magv
-fi
-
-echo "Your key space is $KEYSPACE"
