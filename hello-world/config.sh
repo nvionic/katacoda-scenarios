d39773ae7beb0339e493deca189c2a09a574e62d
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-04-16 15:25:36 -0700

    Place machina in ~/bin instead of ~/.bin

--- hello-world/config.sh
+++ hello-world/config.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 # comment: use command ./download.sh
 
-curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > ~/.bin/machina
-chmod +x ~/.bin/machina
+mkdir ~/bin
+curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > ~/bin/machina
+chmod +x ~/bin/machina
 machina
