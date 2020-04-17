146dc2b5a433e971a9d10c8dd2fe5a4e00a2d251
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-04-17 12:47:29 -0700

    Revert "Use /Users/nvautier/.bin"
    This reverts commit 75c36ec52cb905921839be4035ae87581c5dca01.

--- hello-world/config.sh
+++ hello-world/config.sh
@@ -2,15 +2,15 @@
 # comment: use command ./config.sh
 
 # Downlaod Machina CLI
-mkdir $HOME/.bin
+mkdir $HOME/bin
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > ~/bin/machina
-chmod +x $HOME/.bin/machina
+chmod +x $HOME/bin/machina
 
 # Downlaod the input script that sets the ENV varibles
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/prompt.sh > ~/bin/input.sh
-chmod +x $HOME/.bin/input.sh
+chmod +x $HOME/bin/input.sh
 
 # set PATH so it includes user's private bin if it exists
 #if [ -d "$HOME/bin" ] ; then
-    #PATH="$HOME/bin:$PATH"
+    PATH="$HOME/bin:$PATH"
 #fi
