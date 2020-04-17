eeba56c8ace497a71497416c848b68f92fb98ae2
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-04-17 12:30:43 -0700

    Use /Users/nvautier instead of ~

--- hello-world/config.sh
+++ hello-world/config.sh
@@ -2,13 +2,13 @@
 # comment: use command ./config.sh
 
 # Downlaod Machina CLI
-mkdir ~/bin
+mkdir $HOME/bin
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > ~/bin/machina
-chmod +x ~/bin/machina
+chmod +x $HOME/bin/machina
 
 # Downlaod the input script that sets the ENV varibles
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/prompt.sh > ~/bin/input.sh
-chmod +x ~/bin/input.sh
+chmod +x $HOME/bin/input.sh
 
 # set PATH so it includes user's private bin if it exists
 #if [ -d "$HOME/bin" ] ; then
