63184a3c1db0163fc5528febf2834397b9c7b2c5
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-04-17 14:12:21 -0700

    Revert back to /Users/nvautier/bin

--- hello-world/config.sh
+++ hello-world/config.sh
@@ -3,17 +3,17 @@
 
 # Downlaod Machina CLI
 
-chomd +w $HOME/.bin
+mkdir $HOME/bin
 
 #mkdir $HOME/bin
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > ~/bin/machina
-chmod +x $HOME/.bin/machina
+chmod +x $HOME/bin/machina
 
 # Downlaod the input script that sets the ENV varibles
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/prompt.sh > ~/bin/input.sh
-chmod +x $HOME/.bin/input.sh
+chmod +x $HOME/bin/input.sh
 
 # set PATH so it includes user's private bin if it exists
 #if [ -d "$HOME/bin" ] ; then
-#    PATH="$HOME/bin:$PATH"
+    PATH="$HOME/bin:$PATH"
 #fi
