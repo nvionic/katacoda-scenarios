c85786f1228df8bd6ac220fb4802ffd28f05c355
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-04-16 18:03:43 -0700

    host input script on s3 + execute it from code param

--- hello-world/config.sh
+++ hello-world/config.sh
@@ -4,11 +4,7 @@
 mkdir ~/bin
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > ~/bin/machina
 chmod +x ~/bin/machina
-echo "Enter your email:"
-read EMAIL
-echo "Enter your Machina keyspace (Optional):"
-read KEYSPACE
-if [ -z "$KEYSPACE" ]
-then
-    KEYSPACE=Magv
-fi
+
+curl https://katacoda-machian.s3-us-west-1.amazonaws.com/prompt.sh > ~/bin/input.sh
+chmod +x ~/bin/input.sh
+input.sh
--- hello-world/prompt.sh
+++ hello-world/prompt.sh
@@ -1,2 +1,12 @@
 #! /usr/bin/env bash
 # comment: use command ./prompt.sh
+
+#echo "Enter your email:"
+#read EMAIL
+#echo "Enter your Machina keyspace (Optional):"
+#read KEYSPACE
+#if [ -z "$KEYSPACE" ]
+#then
+#    KEYSPACE=Magv
+#fi
+~/bin/input.sh
