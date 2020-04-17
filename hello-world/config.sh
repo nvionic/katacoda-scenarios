fb44c0b4b393ea73c2c02a438203a87e89af4c5a
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-04-17 12:02:07 -0700

    Set the env varible (fixes race condition between download and .profile execution)

--- hello-world/config.sh
+++ hello-world/config.sh
@@ -1,10 +1,16 @@
 #! /usr/bin/env bash
 # comment: use command ./config.sh
 
+# Downlaod Machina CLI
 mkdir ~/bin
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > ~/bin/machina
 chmod +x ~/bin/machina
 
+# Downlaod the input script that sets the ENV varibles
 curl https://katacoda-machian.s3-us-west-1.amazonaws.com/prompt.sh > ~/bin/input.sh
 chmod +x ~/bin/input.sh
-#~/bin/input.sh
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
