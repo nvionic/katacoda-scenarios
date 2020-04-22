bf7efe20697b01414fd64abc47f49ea2bab35189
Nicolas Vautier <nvautier@ionicsecurity.com>
2020-04-22 10:38:09 -0700

    Added ';' to config script

--- hello-world/config.sh
+++ hello-world/config.sh
@@ -3,19 +3,19 @@
 
 # Downlaod Machina CLI
 
-mkdir $HOME/bin
+mkdir $HOME/bin;
 
 #mkdir $HOME/bin
-curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > $HOME/bin/machina
-chmod +x $HOME/bin/machina
+curl https://katacoda-machian.s3-us-west-1.amazonaws.com/machina > $HOME/bin/machina;
+chmod +x $HOME/bin/machina;
 
-cp $HOME/bin/machina /usr/local/bin
+cp $HOME/bin/machina /usr/local/bin;
 
 # Downlaod the input script that sets the ENV varibles
-curl https://katacoda-machian.s3-us-west-1.amazonaws.com/prompt.sh > $HOME/bin/input.sh
-chmod +x $HOME/bin/input.sh
+curl https://katacoda-machian.s3-us-west-1.amazonaws.com/prompt.sh > $HOME/bin/input.sh;
+chmod +x $HOME/bin/input.sh;
 
 # set PATH so it includes user's private bin if it exists
 #if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
+#    PATH="$HOME/bin:$PATH"
 #fi
